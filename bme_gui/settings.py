ffc06655967d252b59bd70fe61b2fb16502f9928
yanyanming77 <78001469+yanyanming77@users.noreply.github.com>
2025-08-18 20:54:09 -0700

    Update settings.py

--- bme_gui/settings.py
+++ bme_gui/settings.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
