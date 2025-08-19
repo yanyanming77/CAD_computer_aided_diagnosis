8d81e42cc5cbf5a6ec2b424711ca2f02d2e857c3
yanyanming77 <78001469+yanyanming77@users.noreply.github.com>
2025-08-18 21:18:26 -0700

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,7 +1,7 @@
 from cProfile import label
 from dataclasses import fields
 from pyexpat import model
-from turtle import title
+# from turtle import title
 from django import forms
 
 from django.contrib.auth.forms import UserCreationForm
